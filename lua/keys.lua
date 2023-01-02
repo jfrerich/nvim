c37721e466af42c346aae80e7b7cf89f8352352f
Jason Frerich <jason.frerich@mattermost.com>
2023-01-02 10:21:18 -0600

    rename helper func from set to map

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,53 +1,53 @@
 -- replacing keys.vimrc with keys.lua.
 -- remove from keys.vimrc after get it working in this file
 vim.g.mapleader = ","
-local set = vim.keymap.set
+local map = vim.keymap.set
 
-set("n", "ge", '<cmd>NvimTreeToggle<CR>')
+map("n", "ge", '<cmd>NvimTreeToggle<CR>')
 
 -- Split Navigations
-set("n", "<C-J>", "<C-W><C-J>")
-set("n", "<C-K>", "<C-W><C-K>")
-set("n", "<C-L>", "<C-W><C-L>")
-set("n", "<C-H>", "<C-W><C-H>")
+map("n", "<C-J>", "<C-W><C-J>")
+map("n", "<C-K>", "<C-W><C-K>")
+map("n", "<C-L>", "<C-W><C-L>")
+map("n", "<C-H>", "<C-W><C-H>")
 
 -- " Split Windows
-set("n", '<leader>h', '<cmd>split<CR>')
-set("n", '<leader>v', '<cmd>vsplit<CR>')
+map("n", '<leader>h', '<cmd>split<CR>')
+map("n", '<leader>v', '<cmd>vsplit<CR>')
 
 -- Keep cursor at the bottom of the visual selection after you yank it.
-set("v", 'y', 'ygv<Esc>')
+map("v", 'y', 'ygv<Esc>')
 
-set('n', '<leader>n', '<cmd>nohlsearch<CR>')
+map('n', '<leader>n', '<cmd>nohlsearch<CR>')
 
 -- vnoremap <c-a> :Inc<CR> " Increment by 1
-set('v', '<', '<gv') -- better indentation.  doesn't lose visual selection
-set('v', '>', '>gv') -- better indentation.  doesn't lose visual selection
+map('v', '<', '<gv') -- better indentation.  doesn't lose visual selection
+map('v', '>', '>gv') -- better indentation.  doesn't lose visual selection
 
 -- quickfix
-set('n', '<leader>q', ':Quickfix<CR>')
-set('n', '<leader>l', ':Quickfix!<CR>')
+map('n', '<leader>q', ':Quickfix<CR>')
+map('n', '<leader>l', ':Quickfix!<CR>')
 
 -- Folding
-set('n', '<space>', 'zA') -- Enable folding with the spacebar
+map('n', '<space>', 'zA') -- Enable folding with the spacebar
 
-set('n', '<c-a>', ':Inc<CR>') -- Increment by 1
+map('n', '<c-a>', ':Inc<CR>') -- Increment by 1
 
 -- vim-test
-set('n', 't<C-n>', ':TestNearest<CR>')
-set('n', 't<C-f>', ':TestFile<CR>')
-set('n', 't<C-s>', ':TestSuite<CR>')
-set('n', 't<C-l>', ':TestLast<CR>')
-set('n', 't<C-g>', ':TestVisit<CR>')
+map('n', 't<C-n>', ':TestNearest<CR>')
+map('n', 't<C-f>', ':TestFile<CR>')
+map('n', 't<C-s>', ':TestSuite<CR>')
+map('n', 't<C-l>', ':TestLast<CR>')
+map('n', 't<C-g>', ':TestVisit<CR>')
 
 -- vim-asterisk
-set('n', '*', '<Plug>(asterisk-z*)')
-set('n', '#', '<Plug>(asterisk-z#)')
-set('n', 'g*', '<Plug>(asterisk-gz*)')
-set('n', 'g#', '<Plug>(asterisk-gz#)')
+map('n', '*', '<Plug>(asterisk-z*)')
+map('n', '#', '<Plug>(asterisk-z#)')
+map('n', 'g*', '<Plug>(asterisk-gz*)')
+map('n', 'g#', '<Plug>(asterisk-gz#)')
 
 -- Hop - EasyMotion replacement
-set('n', 's', ':HopChar1<CR>')
+map('n', 's', ':HopChar1<CR>')
 
 -- terminal commands
 -- tnoremap <Esc> <C-\><C-n>
