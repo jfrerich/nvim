4961a13f24ef9c689c6f05f5e000976fe39c2cd3
Jason Frerich <jason.frerich@mattermost.com>
2023-01-11 21:53:24 -0600

    Update lua snip to match official lua-snip docs

--- after/plugin/lua-snip.lua
+++ after/plugin/lua-snip.lua
@@ -1,39 +1,23 @@
-local ls = require "luasnip"
-local s = ls.snippet
--- some shorthands...
--- local snip = ls.snippet
--- local node = ls.snippet_node
-local text = ls.text_node
-local insert = ls.insert_node
-local func = ls.function_node
--- local choice = ls.choice_node
--- local dynamicn = ls.dynamic_node
--- local fmt = require("luasnip.extras.fmt").fmt
--- local fmta = require("luasnip.extras.fmt").fmta
--- local rep = require("luasnip.extras").rep
-
 -- Abbreviations used in this article and the LuaSnip docs
-local luasnip = require('luasnip')
-local types = require('luasnip.util.types')
 local extras = require('luasnip.extras')
-local fmt = require('luasnip.extras.fmt').fmt
+local ls = require("luasnip")
 
-luasnip.config.set_config({
+ls.config.set_config({
 	history = false,
 	updateevents = "TextChanged,TextChangedI",
 	snip_env = {
-		s = require("luasnip.nodes.snippet").S,
-		sn = require("luasnip.nodes.snippet").SN,
-		isn = require("luasnip.nodes.snippet").ISN,
-		t = require("luasnip.nodes.textNode").T,
-		i = require("luasnip.nodes.insertNode").I,
-		f = require("luasnip.nodes.functionNode").F,
-		c = require("luasnip.nodes.choiceNode").C,
-		d = require("luasnip.nodes.dynamicNode").D,
-		r = require("luasnip.nodes.restoreNode").R,
+		s = ls.snippet,
+		sn = ls.snippet_node,
+		isn = ls.indent_snippet_node,
+		t = ls.text_node,
+		i = ls.insert_node,
+		f = ls.function_node,
+		c = ls.choice_node,
+		d = ls.dynamic_node,
+		r = ls.restore_node,
 		events = require("luasnip.util.events"),
 		ai = require("luasnip.nodes.absolute_indexer"),
-		extras = extras,
+		extras = require("luasnip.extras"),
 		l = extras.lambda,
 		rep = extras.rep,
 		p = extras.partial,
@@ -49,27 +33,7 @@ luasnip.config.set_config({
 	},
 })
 require("luasnip/loaders/from_lua").lazy_load()
-require("luasnip/loaders/from_lua").lazy_load({ paths = { './snippets' } })
--- ls.config.set_config {
--- }
-
--- ls.setup({
--- 	snip_env = {
--- 		s = function(...)
--- 			local snip = ls.s(...)
--- 			-- we can't just access the global `ls_file_snippets`, since it will be
--- 			-- resolved in the environment of the scope in which it was defined.
--- 			table.insert(getfenv(2).ls_file_snippets, snip)
--- 		end,
--- 		parse = function(...)
--- 			local snip = ls.parser.parse(...)
--- 			table.insert(getfenv(2).ls_file_snippets, snip)
--- 		end,
--- 		-- remaining definitions.
--- 		...
--- 	},
--- 	...
--- })
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets" })
 
 -- local keymap = vim.api.nvim_set_keymap
 -- -- local opts = { noremap = true, silent = true }
@@ -81,10 +45,3 @@ require("luasnip/loaders/from_lua").lazy_load({ paths = { './snippets' } })
 -- keymap("s", "<c-h>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
 -- keymap("i", "<c-l>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
 -- keymap("s", "<c-l>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
-
--- ls.add_snippets(nil, {
--- 	lua = snip("shebang", {
--- 		text { "#!/usr/bin/lua", "", "" },
--- 		insert(0),
--- 	}),
--- })
